1166c003151c19bbad90c0108b3143a30c0f4f8d
LegendaryMagma <39517330+LegendaryMagma@users.noreply.github.com>
2024-07-25 19:22:14 +1000

    update

---
+++ open_X-urls-by-profile-name.py
@@ -0,0 +1,23 @@
+import webbrowser
+
+def open_x_urls(usernames):
+    base_url = "https://x.com/"
+    for username in usernames:
+        url = f"{base_url}{username}"
+        webbrowser.open(url)
+
+if __name__ == "__main__":
+    # List of usernames
+    usernames = [
+        "StreetsYarra",
+        "BristolCycling",
+        "yarrabike",
+        "UserBicycle",
+        "katie_cardigan",
+        "MelbourneWay",
+        "liveable_melb",
+        "bikemelbourne",
+        "WeRideAustralia",
+    ]
+    
+    open_x_urls(usernames)
