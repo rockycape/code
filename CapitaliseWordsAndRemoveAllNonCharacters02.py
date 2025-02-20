97d937203e7b7c3f95bb414997b090d51b96269b
Legendary Magma <39517330+LegendaryMagma@users.noreply.github.com>
2025-02-21 08:25:51 +1100

    update

---
+++ CapitaliseWordsAndRemoveAllNonCharacters02.py
@@ -0,0 +1,13 @@
+import re
+
+def remove_non_chars(text):
+    # Capitalize the first letter of each word
+    capitalized_text = text.title()
+    # The regex pattern [^a-zA-Z] matches any non-letter character.
+    pattern = re.compile(r'[^a-zA-Z]')
+    cleaned_text = re.sub(pattern, '', capitalized_text)
+    return cleaned_text
+
+# Ask for input from the user
+string = input("Enter a string: ")
+print(remove_non_chars(string))
